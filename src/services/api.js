c5972c7bb708e4859741dd07b4826ee93d7c8167
sabari-wq <sabarivenkatesan2962006@gmail.com>
2026-04-01 14:32:11 +0530

    api fixed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { TOKEN_STORAGE_KEY } from '../utils/constants';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
